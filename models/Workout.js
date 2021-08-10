5d5cc27f2b53aa4e2efe0e31ae5e304e79c235fe
Vivi Cowan <vivicare@gmail.com>
2021-08-10 15:49:37 -0700

    created workout model

--- models/Workout.js
+++ models/Workout.js
@@ -1,23 +1,48 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const exerciseSchema = new Schema({
-  type: {
-    type: String,
-    enum: ["Cardio", "Resistance"],
-    required: "Only valid choices are 'Cardio' and 'Resistance'",
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now,
   },
-  name: {
-    type: String,
-    trim: true,
-    required: "Enter a name for the exercise",
-  },
-  duration: {
-    type: Number,
-    required: true,
-  },
-  weight: {
-    type: Number,
-    required: true,
-  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        enum: ["Cardio", "Resistance"],
+        required: "Only valid choices are 'Cardio' and 'Resistance'",
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: "Enter a name for the exercise",
+      },
+      duration: {
+        type: Number,
+        trim: true,
+        required: "Enter a number for the duration of exercise",
+      },
+      weight: {
+        type: Number,
+        trim: true,
+      },
+      reps: {
+        type: Number,
+        trim: true,
+      },
+      sets: {
+        type: Number,
+        trim: true,
+      },
+      distance: {
+        type: Number,
+        trim: true,
+      },
+    },
+  ],
 });
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
